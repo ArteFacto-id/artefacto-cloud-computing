175af39d1faf3c5c0c899aa655dc2f6c95fb6b05
askarhabibulloh <mhmmdaskrhbblh@gmail.com>
2024-11-30 18:54:52 +0700

    checkpoint

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const { artifactRoute } = require('./routes/artifactRoute');
+const Hapi = require('@hapi/hapi');
+require('dotenv').config();
+
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: true
+    }
+  });
+
+  server.route(artifactRoute);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+  console.error('Unhandled Rejection:', err);
+});
+
+init();
